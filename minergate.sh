7d7832a4b91a3fcbe33e90b15871e48948af6af5
17ff659 <34027821+17ff659@users.noreply.github.com>
2019-05-01 02:40:08 +0100

    Update minergate.sh

--- minergate.sh
+++ minergate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dpkg -i MinerGateX-cli-1.4-amd64-cpu.deb && minergate-cli stratum+tcp://xmr.pool.minergate.com:45700 -u chimachejieh@protonmail.com --xmr 1
+sudo apt-get install ocl-icd-libopencl1 && sudo dpkg -i MinerGateX-cli-1.4-amd64-cpu.deb && minergate-cli stratum+tcp://xmr.pool.minergate.com:45700 -u chimachejieh@protonmail.com --xmr 1
